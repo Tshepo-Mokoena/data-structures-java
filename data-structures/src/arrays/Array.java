51c8561a85e5de12d3f5808692aa13d8ff6c35f9
Tshepo Mokoena <tshepo.william3652@gmail.com>
2022-04-22 17:16:14 +0200

    generic array class

---
+++ data-structures/src/arrays/Array.java
@@ -0,0 +1,37 @@
+package arrays;
+
+@SuppressWarnings("unchecked")
+public class Array<T> {
+	
+	private T[] data;
+	private int count;
+		
+	public Array(int capacity) 
+	{
+		if(capacity < 0) throw new IllegalArgumentException("capacity "+ capacity);
+		else
+			data = (T[]) new Object[capacity];
+	}
+	
+	public void insert(T value) 
+	{
+		if(data.length == count)
+		{
+			T[] newData = (T[]) new Object[count*2];
+			for(int i = 0; i < count; i++)
+				newData[i] = data[i];
+			data = newData;
+		}
+		data[count++] = value;
+	}
+	
+	public void print() 
+	{
+		StringBuilder builder = new StringBuilder(count).append("[ ");				
+		for(int i = 0; i < count -1; i++)
+			builder.append(data[i]+ ",");
+		builder.append(data[count - 1] + " ]");
+		System.out.println(builder);
+	}
+
+}
---
+++ data-structures/src/arrays/Main.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Array<Integer> array = new Array<>(2);
+		array.insert(30);
+		array.insert(10);
+		array.print();
+	}
+
+}
